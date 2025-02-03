07480bfbe380f71209ea053d6b5974f6b396680e
Suspicious-Avian <05944@staidans.qld.edu.au>
2025-02-03 14:39:03 +1000

    Update sketch.js
    whoops

--- sketch.js
+++ sketch.js
@@ -372,7 +372,7 @@ function loadchats(chats) {
                 lastMessageCounts[chatID] = messageCount;
             }
         });
-interval
+//interval
         setInterval(() => {
             getCloudVariable('messages', (updatedChats) => {
                 updatedChats.forEach(chat => {
